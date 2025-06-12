d22e891ec16527303cc07f6de2489dadf511b8e5
sreenivas <sreenu.sreenutech@gmail.com>
2025-06-12 07:28:46 +0530

    updated the product file

---
+++ Products.java
@@ -0,0 +1,14 @@
+package com.sreenutech;
+
+@Data
+public class Products{
+	
+	private String id;
+	private String name;
+	private String desc;
+	private String imgUrl;
+	private String qty;
+	private boolean isOffer;
+	
+	
+}
